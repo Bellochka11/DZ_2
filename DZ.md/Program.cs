333ba0545c8b412d0922525db8d550bc37e2341b
Белянкина Елизавета <Belyankina@mail.ru>
2023-03-06 20:51:05 +0300

    создали метод, который формирует массив из массива

--- DZ.md/Program.cs
+++ DZ.md/Program.cs
@@ -1,2 +1,15 @@
-﻿string[] array1 = new string[3] {"2", "5", "hello"};
+﻿string[] array1 = new string[3] { "2", "5", "hello" };
 string[] array2 = new string[array1.Length];
+
+void SecondArrayF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
